62e9d6798bd4a4eaf4125405b52b183b37468a9c
KhunKyawTunwin <66564687+KhunKyawTunwin@users.noreply.github.com>
2022-07-14 21:39:03 +0630

    EDRepo

---
+++ main.js
@@ -0,0 +1,44 @@
+//  Change navbar style on scroll
+
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0);
+})
+
+
+//  Show / Hide FAQ Answer
+const faqs = document.querySelectorAll('.faq');
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+        //  Change Plus to Minus icon
+        const icon = faq.querySelector('.faq_icon i');
+        if (icon.className === 'fa-solid fa-plus') {
+            icon.className = 'fa-solid fa-minus';
+        } else {
+            icon.className = 'fa-solid fa-plus';
+        }
+    })
+})
+
+
+//  Show / Hide Navbar Section
+
+const menu = document.querySelector(".nav_menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+})
+
+//  Close Back Navbar Menu
+const closeNa = () => {
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
+
+}
+
+closeBtn.addEventListener('click', closeNa)
