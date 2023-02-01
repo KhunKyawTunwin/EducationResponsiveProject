910d6b602c3a774d9907bea1a21c623b2899bde1
KhunKyawTunwin <66564687+KhunKyawTunwin@users.noreply.github.com>
2023-02-01 22:45:40 +0630

    CSS New Styles

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
 //  Change navbar style on scroll
 
-window.addEventListener('scroll', () => {
-    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0);
-})
-
+window.addEventListener("scroll", () => {
+  document
+    .querySelector("nav")
+    .classList.toggle("window-scroll", window.scrollY > 0);
+});
 
 //  Show / Hide FAQ Answer
-const faqs = document.querySelectorAll('.faq');
-faqs.forEach(faq => {
-    faq.addEventListener('click', () => {
-        faq.classList.toggle('open');
-        //  Change Plus to Minus icon
-        const icon = faq.querySelector('.faq_icon i');
-        if (icon.className === 'fa-solid fa-plus') {
-            icon.className = 'fa-solid fa-minus';
-        } else {
-            icon.className = 'fa-solid fa-plus';
-        }
-    })
-})
-
+const faqs = document.querySelectorAll(".faq");
+faqs.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("open");
+    //  Change Plus to Minus icon
+    const icon = faq.querySelector(".faq_icon i");
+    if (icon.className === "fa-solid fa-plus") {
+      icon.className = "fa-solid fa-minus";
+    } else {
+      icon.className = "fa-solid fa-plus";
+    }
+  });
+});
 
 //  Show / Hide Navbar Section
 
@@ -27,18 +27,17 @@ const menu = document.querySelector(".nav_menu");
 const menuBtn = document.querySelector("#open-menu-btn");
 const closeBtn = document.querySelector("#close-menu-btn");
 
-menuBtn.addEventListener('click', () => {
-    menu.style.display = "flex";
-    closeBtn.style.display = "inline-block";
-    menuBtn.style.display = "none";
-})
+menuBtn.addEventListener("click", () => {
+  menu.style.display = "flex";
+  closeBtn.style.display = "inline-block";
+  menuBtn.style.display = "none";
+});
 
 //  Close Back Navbar Menu
 const closeNa = () => {
-    menu.style.display = "none";
-    closeBtn.style.display = "none";
-    menuBtn.style.display = "inline-block";
+  menu.style.display = "none";
+  closeBtn.style.display = "none";
+  menuBtn.style.display = "inline-block";
+};
 
-}
-
-closeBtn.addEventListener('click', closeNa)
+closeBtn.addEventListener("click", closeNa);
